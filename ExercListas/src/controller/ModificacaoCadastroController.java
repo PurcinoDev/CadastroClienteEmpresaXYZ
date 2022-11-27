7189aa9bc31294ab6cb2c04a1aff0d0fded78bb5
PurcinoDev <111099526+PurcinoDev@users.noreply.github.com>
2022-11-27 15:36:48 -0300

    Update ModificacaoCadastroController.java

--- ExercListas/src/controller/ModificacaoCadastroController.java
+++ ExercListas/src/controller/ModificacaoCadastroController.java
@@ -16,12 +16,41 @@ public class ModificacaoCadastroController {
 		super();
 	}
 	
-	private void novoArquivo(Lista l, String nomeArquivo) throws IOException {
+	private void novoArquivo(Lista l, String caminho, String nomeArquivo) throws Exception {
+		
+		File dir = new File(caminho);
+		
+		if (dir.exists() && dir.isDirectory()) {
+			
+//			Cria um novo arquivo
+			File arq = new File(caminho, nomeArquivo);
+			
+//			Instancia um StringBuffer para preencher o arquivo com o conteúdo da lista
+			StringBuffer buffer = new StringBuffer();
+			int contador = l.size();
+			String linha = "";
+			for (int i = 0; i < contador; i++) {
+				linha = l.get(i).toString();
+				buffer.append(linha + "\r\n");
+			}
+			
+//			Escreve o conteúdo pego no StringBuffer no arquivo novo criado
+			FileWriter abreArquivo = new FileWriter(arq);
+			PrintWriter escreveArq = new PrintWriter(abreArquivo);
+			escreveArq.write(buffer.toString());
+			escreveArq.flush();
+			escreveArq.close();
+			abreArquivo.close();
+		} else {
+			throw new IOException("Diretório inválido");
+		}
+		
 	}
 	
-	public void novoCadastro(int idadeMin, int idadeMax, double limiteCredito) throws IOException {
+	public void novoCadastro(String caminho, String arquivo, int idadeMin, int idadeMax, double limiteCredito) throws Exception {
 		Lista lClient = new Lista();
-		File arq = new File("C:\\TEMP", "cadastro.csv");
+		File arq = new File(caminho, arquivo);
+		
 		if (arq.exists() && arq.isFile()) {
 			FileInputStream fluxo = new FileInputStream(arq);
 			InputStreamReader leFluxo = new InputStreamReader(fluxo);
@@ -34,21 +63,26 @@ public class ModificacaoCadastroController {
 					cliente.cpf = vetLinha[0];
 					cliente.nome = vetLinha[1];
 					cliente.idade = Integer.parseInt(vetLinha[2]);
-					cliente.limiteCredito = Double.parseDouble(vetLinha[3]);
-					if ((cliente.idade >= idadeMin && cliente.idade <= idadeMax) && cliente.limiteCredito >= limiteCredito) {
-						lClient.addFirst(cliente);
+					cliente.limiteCredito = Double.parseDouble(vetLinha[3].replace(",", "."));
+					if (cliente.idade >= idadeMin && cliente.idade <= idadeMax && cliente.limiteCredito >= limiteCredito) {
+						if (lClient.isEmpty()) {
+							lClient.addFirst(cliente);
+						} else {
+							lClient.addLast(cliente);
+						}
 					}
 				}
+				
 				linha = buffer.readLine();
 			}
 			buffer.close();
 			leFluxo.close();
 			fluxo.close();
 		} else {
-			throw new IOException("Arquivo inv�lido");
+			throw new IOException("Arquivo inválido");
 		}
-		String nomeArquivo = "idade" + idadeMin + " limite" + limiteCredito;
-		novoArquivo(lClient, nomeArquivo);
+		String nomeArquivo = "Idade" + idadeMin + "_Limite" + (int)limiteCredito + ".csv";
+		novoArquivo(lClient, caminho, nomeArquivo);
 	}
 
 }
